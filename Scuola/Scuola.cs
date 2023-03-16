64fea4115ee34def0159bc40ca829ba743f6a831
Diego Bernabini <DIEGO.BERNABINI@local.ittcesena.it>
2023-03-16 08:53:53 +0100

    versione 1.2: aggiunta dei get e dei set nel branch scuola

--- Scuola/Scuola.cs
+++ Scuola/Scuola.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Scuola
 {
-    class Scuola
+   public class Scuola
     {
+        Alunno[] listaAlunni { get; set; }
+        public string Nome { get; set; }
+        public string Inidirizzo { get; set; }
+        public string Apertura { get; set; }
+        public string Chiusura { get; set; }
+
+        //sinceramente non capisco se devo creare un metodo per memorizzare dati o solo le strutture necessare per contenerli,
+        //dato che ogni parte della classe è settabile.
+
     }
 }
